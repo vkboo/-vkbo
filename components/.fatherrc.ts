0b8c8302885e4651086b98f8ae1b37d6c8b8a3e4
bo.zhao <bo.zhao@iglooinsure.com>
2022-06-29 18:33:10 +0800

    fix: add babel-plugin-import in fatherrc

--- components/.fatherrc.ts
+++ components/.fatherrc.ts
@@ -1,3 +1,16 @@
 export default {
+  target: 'browser',
   esm: 'babel',
+  cjs: 'babel',
+  include: 'node_modules/**',
+  extraBabelPlugins: [
+    [
+      'babel-plugin-import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+      },
+    ],
+  ],
 };
