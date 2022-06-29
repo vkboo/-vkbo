e6e53e5c292f57da52a7b0fa8a6932800e901e83
bo.zhao <bo.zhao@iglooinsure.com>
2022-06-29 17:56:01 +0800

    fix: chore

--- components/src/Text/index.tsx
+++ components/src/Text/index.tsx
@@ -1,9 +1,9 @@
 import React, { FC } from 'react';
 
-const Text: FC = ({ children }) => {
-  const characters = [undefined, null, ''];
-  let text = characters.includes(children as string) ? '-' : children;
-  return <>{text}</>;
+const Text: FC<{ children: string }> = ({ children }) => {
+    const characters = [undefined, null, ''];
+    let text = characters.includes(children) ? '-' : children;
+    return <>{text}</>;
 };
 
 export default Text;
